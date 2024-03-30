a11a2ba86a47f368bc2e13398b2ba9e2a9844919
Carmelito Jr <carmelito.benali@hotmail.com>
2024-03-30 13:25:17 -0300

    Projection of author data with DTO

--- src/main/java/springbootmongodb/mongodb/config/Instantiation.java
+++ src/main/java/springbootmongodb/mongodb/config/Instantiation.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import springbootmongodb.mongodb.domain.Post;
 import springbootmongodb.mongodb.domain.User;
+import springbootmongodb.mongodb.dto.AuthorDTO;
 import springbootmongodb.mongodb.repository.PostRepository;
 import springbootmongodb.mongodb.repository.UserRepository;
 
@@ -34,18 +35,19 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        repository.saveAll(Arrays.asList(maria, alex, bob));
+
         Post post1 = new Post(null,
                 sdf.parse("21/03/2018"),
                 "Partiu viagem",
                 "Vou viajar para São Paulo. Abraços!",
-                maria);
+                new AuthorDTO(maria));
         Post post2 = new Post(null,
                 sdf.parse("23/03/2018"),
                 "Bom dia",
                 "Acordei Feliz hoje!",
-                maria);
+                new AuthorDTO(maria));
 
-        repository.saveAll(Arrays.asList(maria, alex, bob));
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 
