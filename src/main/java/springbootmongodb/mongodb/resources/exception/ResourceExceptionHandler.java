b948e31dbad08d6593e194fe65918ac02f7d2c3d
Carmelito Jr <carmelito.benali@hotmail.com>
2024-03-30 09:54:57 -0300

    Find user By Id

---
+++ src/main/java/springbootmongodb/mongodb/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package springbootmongodb.mongodb.resources.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import springbootmongodb.mongodb.service.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandarError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandarError err = new StandarError(
+               System.currentTimeMillis(),
+               status.value(),
+               "Não Encontrado",
+               e.getMessage(),
+               request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/springbootmongodb/mongodb/service/UserService.java
+++ src/main/java/springbootmongodb/mongodb/service/UserService.java
@@ -4,16 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import springbootmongodb.mongodb.domain.User;
 import springbootmongodb.mongodb.repository.UserRepository;
+import springbootmongodb.mongodb.service.exception.ObjectNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository repository;
+
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
 }
